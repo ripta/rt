def2716c358366730e80d95e1b7fc18c0cb6ee98
Ripta Pasay <ripta@users.noreply.github.com>
2022-01-02 22:29:18 +0000

    pkg/enc: base64 encoding

--- pkg/enc/base64.go
+++ pkg/enc/base64.go
@@ -1,26 +1,39 @@
 package enc
 
 import (
-	"fmt"
+	"encoding/base64"
+	"io"
 
 	"github.com/spf13/cobra"
 )
 
-type base64Coder struct {}
-
-func newBase64Command() *cobra.Command {
-	r := &base64Coder{}
+func newBase64Command(e *encoder) *cobra.Command {
 	c := cobra.Command{
-		Use:         "base64",
-		Aliases:     []string{"b64"},
-		Description: "Base64",
-		RunE: r.run,
+		Use:     "base64",
+		Aliases: []string{"b64"},
+		Short:   "Base64",
+		RunE:    e.choose(e.b64Encode, e.b64Decode),
 	}
 
 	return &c
 }
 
-func (r *base64Coder) run(cmd *cobra.Command, args []string) error {
-	fmt.Println(cmd.Name())
+func (e *encoder) b64Decode(dst io.Writer, src io.Reader) error {
+	br := base64.NewDecoder(base64.StdEncoding.WithPadding(base64.NoPadding), src)
+	if _, err := io.Copy(dst, br); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (e *encoder) b64Encode(dst io.Writer, src io.Reader) error {
+	bw := base64.NewEncoder(base64.StdEncoding, dst)
+	defer bw.Close()
+
+	if _, err := io.Copy(bw, src); err != nil {
+		return err
+	}
+
 	return nil
 }
--- pkg/enc/enc.go
+++ pkg/enc/enc.go
@@ -1,17 +1,36 @@
 package enc
 
-import "github.com/spf13/cobra"
+import (
+	"io"
+	"os"
 
-func AddCommands(c *cobra.Command) *cobra.Command {
-	c.AddCommand(newBase64Command())
-	return c
+	"github.com/spf13/cobra"
+)
+
+type encoder struct {
+	Decode bool
+}
+
+func (e *encoder) choose(enc, dec func(io.Writer, io.Reader) error) func(*cobra.Command, []string) error {
+	return func(cmd *cobra.Command, args []string) error {
+		w, r := os.Stdout, os.Stdin
+
+		if e.Decode {
+			return dec(w, r)
+		}
+		return enc(w, r)
+	}
 }
 
 func NewCommand() *cobra.Command {
+	e := &encoder{}
 	c := cobra.Command{
 		Use:   "enc",
-		Short: "Encoding / decoding commands",
+		Short: "Encodings (and their decodings)",
 	}
 
-	return AddCommands(&c)
+	c.PersistentFlags().BoolVarP(&e.Decode, "decode", "d", false, "Decode instead of encode")
+
+	c.AddCommand(newBase64Command(e))
+	return &c
 }
