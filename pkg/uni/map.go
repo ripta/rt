73d8e4fc0cab1eee021f84a58c50345a57e8b05f
Ripta Pasay <ripta@users.noreply.github.com>
2022-11-16 23:01:29 -0800

    pkg/uni: allow viewing all mapschemes at once

--- pkg/uni/map.go
+++ pkg/uni/map.go
@@ -3,7 +3,7 @@ package uni
 import (
 	"errors"
 	"fmt"
-	"io/ioutil"
+	"io"
 	"os"
 	"strings"
 
@@ -29,19 +29,54 @@ func newMapCommand() *cobra.Command {
 		RunE:  m.run,
 	}
 
+	c.Flags().BoolVarP(&m.ShowAll, "all", "a", m.ShowAll, "Show all schemes")
+
 	return &c
 }
 
 type mapper struct {
+	ShowAll bool
 }
 
 func (m *mapper) run(c *cobra.Command, args []string) error {
-	name := args[0]
+	if m.ShowAll {
+		return m.runAll(os.Stdin)
+	}
+	return m.runOne(os.Stdin, args[0])
+}
+
+func (m *mapper) runAll(r io.Reader) error {
+	in, err := io.ReadAll(r)
+	if err != nil {
+		return err
+	}
+
+	lines := strings.Split(strings.TrimSpace(string(in)), "\n")
+	if len(lines) != 1 {
+		return fmt.Errorf("expected one line of input, but received %d", len(lines))
+	}
+
+	orig := strings.TrimSpace(lines[0])
+	if orig == "" {
+		orig = "The lazy brown fox jumps over the quick dog" // 😉
+	}
+
+	fmt.Printf("original: %s\n", orig)
+
+	for _, name := range mapscheme.Names() {
+		scheme := mapscheme.Get(name)
+		fmt.Printf("%s: %s\n", name, strings.Map(scheme, orig))
+	}
+
+	return nil
+}
+
+func (m *mapper) runOne(r io.Reader, name string) error {
 	scheme := mapscheme.Get(name)
 
-	in, err := ioutil.ReadAll(os.Stdin)
+	in, err := io.ReadAll(r)
 	if err != nil {
-		return fmt.Errorf("reading from stdin: %w", err)
+		return err
 	}
 
 	out := strings.Map(scheme, string(in))
@@ -50,6 +85,10 @@ func (m *mapper) run(c *cobra.Command, args []string) error {
 }
 
 func (m *mapper) validate(_ *cobra.Command, args []string) error {
+	if m.ShowAll {
+		return nil
+	}
+
 	if len(args) != 1 {
 		return fmt.Errorf("%w, available %+v", ErrMapSchemeRequired, mapscheme.Names())
 	}
