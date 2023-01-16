7c85bb652a493288a7dc135e5ebd2f721c16bd75
Ripta Pasay <ripta@users.noreply.github.com>
2023-01-15 22:17:19 -0800

    pkg/location: fix all incompatible pointer types (by using unsafe code… 👀)

--- pkg/location/macstrings.go
+++ pkg/location/macstrings.go
@@ -2,18 +2,18 @@
 
 package location
 
-// #cgo CFLAGS: -x objective-c
+// #cgo CFLAGS: -x objective-c -Wno-incompatible-pointer-types
 // #cgo LDFLAGS: -framework Foundation
 // #include "macstrings.h"
 import "C"
 import "unsafe"
 
 func NSArrayLength(arr *C.NSArray) uint {
-	return uint(C.nsarray_length(arr))
+	return uint(C.nsarray_length(unsafe.Pointer(arr)))
 }
 
 func NSArrayObjectAtIndex(arr *C.NSArray, idx uint) unsafe.Pointer {
-	return C.nsarray_object_at_index(arr, C.ulong(idx))
+	return C.nsarray_object_at_index(unsafe.Pointer(arr), C.ulong(idx))
 }
 
 func NSArrayNSStringToGoStringSlice(arr *C.NSArray) []string {
@@ -26,5 +26,5 @@ func NSArrayNSStringToGoStringSlice(arr *C.NSArray) []string {
 }
 
 func NSStringToGoString(str *C.NSString) string {
-	return C.GoString(C.nsstring_to_charstar(str))
+	return C.GoString(C.nsstring_to_charstar(unsafe.Pointer(str)))
 }
--- pkg/location/macstrings.m
+++ pkg/location/macstrings.m
@@ -1,24 +1,27 @@
 #import "macstrings.h"
 
-unsigned long nsarray_length(NSArray *arr) {
+unsigned long nsarray_length(void* arr) {
   if (arr == NULL) {
     return 0;
   }
-  return arr.count;
+  NSArray *na = *((__unsafe_unretained NSArray **)(arr));
+  return na.count;
 }
 
-const void *nsarray_object_at_index(NSArray *arr, unsigned long idx) {
+const void *nsarray_object_at_index(void* arr, unsigned long idx) {
   if (arr == NULL) {
     return NULL;
   }
-  return [arr objectAtIndex:idx];
+  NSArray *na = *((__unsafe_unretained NSArray **)(arr));
+  return [na objectAtIndex:idx];
 }
 
-const char *nsstring_to_charstar(NSString *str) {
+const char *nsstring_to_charstar(void* str) {
   if (str == NULL) {
     return NULL;
   }
 
-  const char *cs = [str UTF8String];
+  NSString *s = *((__unsafe_unretained NSString **)(str));
+  const char *cs = [s UTF8String];
   return cs;
 }
