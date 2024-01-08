9520587de8f46de0f4c68232efaf3c121009303a
Ripta Pasay <ripta@users.noreply.github.com>
2024-01-07 23:46:45 -0800

    pkg/uni: refactor normal forms by combining into a generating function
    Additionally, fixes the short description of NFC, and updates the
    deprecated use of ioutil.ReadAll with io.ReadAll.

---
+++ pkg/uni/forms.go
@@ -0,0 +1,45 @@
+package uni
+
+import (
+	"fmt"
+	"io"
+	"os"
+
+	"github.com/spf13/cobra"
+	"golang.org/x/text/unicode/norm"
+)
+
+func newNFCCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:     "nfc",
+		Aliases: []string{"com", "comp", "compose"},
+		Short:   "Output the canonical composition form for input",
+		RunE:    generateForm(norm.NFC),
+	}
+}
+
+func newNFDCommand() *cobra.Command {
+	return &cobra.Command{
+		Use:     "nfd",
+		Aliases: []string{"dec", "decompose"},
+		Short:   "Output the canonical decomposition form for input",
+		RunE:    generateForm(norm.NFD),
+	}
+}
+
+func generateForm(form norm.Form) func(*cobra.Command, []string) error {
+	return func(_ *cobra.Command, args []string) error {
+		in, err := io.ReadAll(os.Stdin)
+		if err != nil {
+			return fmt.Errorf("reading from stdin: %w", err)
+		}
+
+		n := norm.Iter{}
+		n.Init(form, in)
+		for !n.Done() {
+			fmt.Print(string(n.Next()))
+		}
+
+		return nil
+	}
+}
