6247905937d8cbb69d9d19801763538f0d49d29f
Ripta Pasay <ripta@users.noreply.github.com>
2025-04-03 01:07:47 -0700

    pkg/structfiles: implement EDNDecoder with forced stringification (will i regret this?)

--- pkg/structfiles/manager/format_edn.go
+++ pkg/structfiles/manager/format_edn.go
@@ -1,20 +1,40 @@
 package manager
 
 import (
+	"fmt"
 	"io"
-	"olympos.io/encoding/edn"
+	"reflect"
 	"strings"
+
+	"olympos.io/encoding/edn"
 )
 
 func init() {
 	enc := &EDNEncoder{}
-	RegisterFormatWithOptions("edn", []string{".edn"}, enc.EncodeTo, enc, nil, nil)
+	RegisterFormatWithOptions("edn", []string{".edn"}, enc.EncodeTo, enc, EDNDecoder, nil)
 }
 
 // EDNDecoder currently decodes any key, not just string, so it behaves differently
 // from other decoders.
 func EDNDecoder(r io.Reader) Decoder {
-	return edn.NewDecoder(r)
+	dec := edn.NewDecoder(r)
+	return DecoderFunc(func(v any) error {
+		rv := reflect.ValueOf(v)
+		if rv.Kind() != reflect.Ptr || rv.IsNil() {
+			return fmt.Errorf("expected a pointer to a struct, got %T", v)
+		}
+
+		av := map[any]any{}
+		if err := dec.Decode(&av); err != nil {
+			return err
+		}
+
+		sv := map[string]any{}
+		ConvertAnyMapToStringMapRecursive(sv, av)
+		rv.Elem().Set(reflect.ValueOf(sv))
+
+		return nil
+	})
 }
 
 type EDNEncoder struct {
