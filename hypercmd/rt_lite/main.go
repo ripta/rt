88eac0b0898713f5ee68f9ff54148e49cc6b8537
Ripta Pasay <ripta@users.noreply.github.com>
2025-11-23 00:46:57 -0800

    hypercmd/rt_lite: lighter version without streaming or terminal support

---
+++ hypercmd/rt_lite/main.go
@@ -0,0 +1,42 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/ripta/hypercmd/pkg/hypercmd"
+
+	"github.com/ripta/rt/pkg/enc"
+	"github.com/ripta/rt/pkg/hashsum"
+	"github.com/ripta/rt/pkg/uni"
+	"github.com/ripta/rt/pkg/version"
+	"github.com/ripta/rt/pkg/yfmt"
+)
+
+func main() {
+	root := hypercmd.New("rt")
+	root.Root().SilenceErrors = true
+	root.Root().SilenceUsage = true
+
+	root.AddCommand(enc.NewCommand())
+	root.AddCommand(hashsum.NewCommand())
+	root.AddCommand(uni.NewCommand())
+	root.AddCommand(yfmt.NewCommand())
+
+	v := version.NewCommand()
+	root.Root().AddCommand(v)
+	for _, c := range root.Commands() {
+		c.AddCommand(v)
+	}
+
+	cmd, err := root.Resolve(os.Args, true)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %+v\n", err)
+		os.Exit(1)
+	}
+
+	if err := cmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error: %+v\n", err)
+		os.Exit(1)
+	}
+}
