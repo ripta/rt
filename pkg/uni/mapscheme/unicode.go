182d204f2dee4ba1c9ab6eddbce28a903ae08246
Ripta Pasay <ripta@users.noreply.github.com>
2025-12-23 21:38:07 +0000

    pkg/uni: add mapscheme for togglecase

--- pkg/uni/mapscheme/unicode.go
+++ pkg/uni/mapscheme/unicode.go
@@ -6,4 +6,12 @@ func init() {
 	registry["lower"] = unicode.ToLower
 	registry["title"] = unicode.ToTitle
 	registry["upper"] = unicode.ToUpper
+	registry["toggle"] = func(r rune) rune {
+		if unicode.IsLower(r) {
+			return unicode.ToUpper(r)
+		} else if unicode.IsUpper(r) {
+			return unicode.ToLower(r)
+		}
+		return r
+	}
 }
