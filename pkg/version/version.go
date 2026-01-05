b269e8dafa18361cda2ba0aea2d4858ca763c4ae
Ripta Pasay <ripta@users.noreply.github.com>
2026-01-05 14:38:00 -0800

    pkg/version: refactor to allow the version to be queried without invoking command

--- pkg/version/version.go
+++ pkg/version/version.go
@@ -9,6 +9,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
+// Version is a build-time variable that users can set to override the version string.
+//
+// It can be set using: -ldflags "-X github.com/ripta/rt/pkg/version.Version=1.2.3"
 var Version string
 
 type versioner struct {
@@ -30,30 +33,34 @@ func NewCommand() *cobra.Command {
 	return &c
 }
 
-type VersionInfo struct {
+// Info contains version information about the build.
+type Info struct {
+	// BuildInfo contains the Go build information from the binary
 	*debug.BuildInfo
+	// Version is the formatted version string
 	Version string
 }
 
-func (v *versioner) run(cmd *cobra.Command, args []string) error {
+// Get retrieves the version information from the build info. This relies on
+// the vcs.revision and vcs.modified build settings. If the binary was built
+// without module support, it returns an error.
+func Get() (*Info, error) {
+	_ = Version // mark used
+
 	bi, ok := debug.ReadBuildInfo()
 	if !ok {
-		return errors.New("version not available: binary was not built with module support")
+		return nil, errors.New("version not available: binary was not built with module support")
 	}
 
 	version := bi.Main.Version
 	dirty := false
 	if version == "(devel)" {
-		if Version != "" {
-			version = Version
-		} else {
-			for _, s := range bi.Settings {
-				if s.Key == "vcs.revision" {
-					version = s.Value
-				}
-				if s.Key == "vcs.modified" && s.Value == "true" {
-					dirty = true
-				}
+		for _, s := range bi.Settings {
+			if s.Key == "vcs.revision" {
+				version = s.Value
+			}
+			if s.Key == "vcs.modified" && s.Value == "true" {
+				dirty = true
 			}
 		}
 	}
@@ -62,11 +69,40 @@ func (v *versioner) run(cmd *cobra.Command, args []string) error {
 		version = version + "-dirty"
 	}
 
+	return &Info{
+		BuildInfo: bi,
+		Version:   version,
+	}, nil
+}
+
+// GetString returns the version as a string. If the Version package variable
+// is set, it returns that value. Otherwise, it attempts to get the version
+// from the build info. If _that_ fails, it returns empty string, which you
+// may presumably want to handle.
+func GetString() string {
+	if Version != "" {
+		return Version
+	}
+
+	vi, err := Get()
+	if err != nil {
+		return ""
+	}
+
+	return vi.Version
+}
+
+func (v *versioner) run(cmd *cobra.Command, args []string) error {
+	vi, err := Get()
+	if err != nil {
+		return err
+	}
+
+	if Version != "" {
+		vi.Version = Version
+	}
+
 	if v.JSON {
-		vi := VersionInfo{
-			BuildInfo: bi,
-			Version:   version,
-		}
 		bs, err := json.MarshalIndent(&vi, "", "  ")
 		if err != nil {
 			return err
@@ -76,6 +112,6 @@ func (v *versioner) run(cmd *cobra.Command, args []string) error {
 		return nil
 	}
 
-	fmt.Printf("%s version %s\n", bi.Path, version)
+	fmt.Printf("%s version %s\n", vi.BuildInfo.Path, vi.Version)
 	return nil
 }
